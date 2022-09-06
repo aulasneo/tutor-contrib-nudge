b58f70b462dd6140288ee4bbea0ddd0851d12c3d
Andrés González <andres@aulasneo.com>
2022-09-06 12:18:27 -0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,37 @@
+nudge plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Plugin to enable nudge and highlights emails.
+
+It creates a CronJob in K8s based on the openedx docker image that calls
+the `send_course_update` and `send_recurring_nudge` management commands
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/aulasneo/tutor-contrib-nudge
+
+Configuration
+-------------
+
+* NUDGE_SCHEDULE: Set the schedule for the nudge emails. Default is "0 10 * * *" (everyday at 10am).
+
+* NUDGE_SEND_COURSE_UPDATE: Enable course updates. Default is True.
+
+* NUDGE_SEND_RECURRING_NUDGE: Enable recurring nudges. Default is True.
+
+
+Usage
+-----
+
+::
+
+    tutor plugins enable nudge
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ tutornudge/plugin.py
@@ -0,0 +1,120 @@
+from glob import glob
+import os
+import pkg_resources
+
+from tutor import hooks
+
+from .__about__ import __version__
+
+
+########################################
+# CONFIGURATION
+########################################
+
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        # Add your new settings that have default values here.
+        # Each new setting is a pair: (setting_name, default_value).
+        # Prefix your setting names with 'NUDGE_'.
+        ("NUDGE_VERSION", __version__),
+        ("NUDGE_SCHEDULE", "0 10 * * *"),
+        ("NUDGE_SEND_COURSE_UPDATE", True),
+        ("NUDGE_SEND_RECURRING_NUDGE", True),
+    ]
+)
+
+hooks.Filters.CONFIG_UNIQUE.add_items(
+    [
+        # Add settings that don't have a reasonable default for all users here.
+        # For instance: passwords, secret keys, etc.
+        # Each new setting is a pair: (setting_name, unique_generated_value).
+        # Prefix your setting names with 'NUDGE_'.
+        # For example:
+        # ("NUDGE_SECRET_KEY", "{{ 24|random_string }}"),
+    ]
+)
+
+hooks.Filters.CONFIG_OVERRIDES.add_items(
+    [
+        # Danger zone!
+        # Add values to override settings from Tutor core or other plugins here.
+        # Each override is a pair: (setting_name, new_value). For example:
+        # ("PLATFORM_NAME", "My platform"),
+    ]
+)
+
+
+########################################
+# INITIALIZATION TASKS
+########################################
+
+# To run the script from templates/nudge/tasks/myservice/init, add:
+# hooks.Filters.COMMANDS_INIT.add_item((
+#     "myservice",
+#     ("nudge", "tasks", "myservice", "init"),
+# ))
+
+
+########################################
+# DOCKER IMAGE MANAGEMENT
+########################################
+
+# To build an image with `tutor images build myimage`, add a Dockerfile to templates/nudge/build/myimage and write:
+# hooks.Filters.IMAGES_BUILD.add_item((
+#     "myimage",
+#     ("plugins", "nudge", "build", "myimage"),
+#     "docker.io/myimage:{{ NUDGE_VERSION }}",
+#     (),
+# )
+
+# To pull/push an image with `tutor images pull myimage` and `tutor images push myimage`, write:
+# hooks.Filters.IMAGES_PULL.add_item((
+#     "myimage",
+#     "docker.io/myimage:{{ NUDGE_VERSION }}",
+# )
+# hooks.Filters.IMAGES_PUSH.add_item((
+#     "myimage",
+#     "docker.io/myimage:{{ NUDGE_VERSION }}",
+# )
+
+
+########################################
+# TEMPLATE RENDERING
+# (It is safe & recommended to leave
+#  this section as-is :)
+########################################
+
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_items(
+    # Root paths for template files, relative to the project root.
+    [
+        pkg_resources.resource_filename("tutornudge", "templates"),
+    ]
+)
+
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    # For each pair (source_path, destination_path):
+    # templates at ``source_path`` (relative to your ENV_TEMPLATE_ROOTS) will be
+    # rendered to ``destination_path`` (relative to your Tutor environment).
+    [
+        ("nudge/build", "plugins"),
+        ("nudge/apps", "plugins"),
+    ],
+)
+
+
+########################################
+# PATCH LOADING
+# (It is safe & recommended to leave
+#  this section as-is :)
+########################################
+
+# For each file in tutornudge/patches,
+# apply a patch based on the file's name and contents.
+for path in glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutornudge", "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
