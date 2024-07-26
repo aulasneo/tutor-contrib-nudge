d7f0a343eb679c50a7006666e89b0a661f2a365b
Andrés González <andres@aulasneo.com>
2024-07-26 16:56:51 -0300

    feat: Upgrade to Redwood

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=["tutor >= 17.0.0, < 18.0.0"],
+    install_requires=["tutor >= 18.0.0, < 19.0.0"],
     entry_points={
         "tutor.plugin.v1": [
             "nudge = tutornudge.plugin"
